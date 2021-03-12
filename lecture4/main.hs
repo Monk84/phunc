e289b6a3b8cde16b3b3247330948b82e7e2f3277
Monk <42776976+Monk84@users.noreply.github.com>
2021-03-12 15:48:47 +0300

    Create main.hs
    Без КР в конце лекции 4

---
+++ lecture4/main.hs
@@ -0,0 +1,76 @@
+-- НОД
+mgcd x y =
+	let mygcd a b
+		| a < 0 || b < 0 = 0
+		| a == 0 = b
+		| b == 0 = a
+		| a == b = a
+		| a > b = mygcd (a-b) b
+		| otherwise = mygcd a (b-a)
+	in
+		mygcd x y
+
+-- Функция delete :: Char -> String -> String, 
+-- которая принимает на вход строку и символ и возвращает строку, 
+-- в которой удалены все вхождения символа. 
+-- Пример: delete ’l’ "Hello world!" должно возвращать "Heo word!".
+delete' c [] = []
+delete' c (s:ss)
+	| s == c = delete' c ss
+	| otherwise = ( s ) : ( delete' c ss )
+
+-- Функция substitute :: Char -> Char -> String -> String, 
+-- которая заменяет в строке указанный символ на заданный. 
+-- Пример: substitute ’e’ ’i’ "eigenvalue" возвращает "iiginvalui"
+substitute' cha chb [] = []
+substitute' cha chb (s:ss)
+  | s == chb = ( cha ) : ( substitute' cha chb ss )
+  | otherwise = ( s ) : ( substitute' cha chb ss )
+
+-- Числа Фибоначчи
+fib n
+	| n < ( -1 ) = ( fib ( n + 2 ) ) - ( fib ( n + 1 ) )
+	| n == -1 = 1
+	| n == 0 = 0
+	| n == 1 = 1
+	| otherwise = ( fib ( n - 1 ) ) + ( fib ( n - 2 ) )
+
+-- Обращение списка
+reverse' [] = []
+reverse' [ a ] = [ a ]
+reverse' (s:ss) = ( reverse' ss ) ++ [ s ]
+
+reverse'' list =
+	let
+		rev [] acc = acc
+		rev (x:xs) acc = rev xs (x:acc)
+	in
+		rev list []
+
+-- Подсчет баланса скобок в строке
+brbal str = 
+	let
+		bal (s:ss) br
+			| br < 0 = False
+			| s == '(' = bal ss ( br + 1 )
+			| s == ')' = bal ss ( br - 1 )
+		bal [] br
+			| br == 0 = True
+			| otherwise = False
+	in
+		bal str 0
+
+-- разворачивает списочную структуру и все списки в ней
+reverseAll list = reverse' ( map reverse' list )
+
+-- вовзращает позицию первого совпадающего с el элемента списка list
+firstElem el list =
+	let
+		srch num el [] = 0
+		srch num el [s] 
+			| el == s = ( num + 1 )
+ 		srch num el (s:ss)
+			| el == s = ( num + 1 )
+			| otherwise = srch ( num + 1 ) el ss
+	in
+		srch 0 el list
