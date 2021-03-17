a9a77348c6982f03a3bc35a653438d6b5b782113
Monk <42776976+Monk84@users.noreply.github.com>
2021-03-17 12:59:51 +0300

    Update main.hs

--- lecture5/main.hs
+++ lecture5/main.hs
@@ -1,1 +1,21 @@
+-- import
+
+-- Список чисел Фибоначчи, от первого до n-го включительно
+fib2 n =
+	let
+		fib n
+			| n < ( -1 ) = ( fib ( n + 2 ) ) - ( fib ( n + 1 ) )
+			| n == -1 = 1
+			| n == 0 = 0
+			| n == 1 = 1
+			| otherwise = ( fib ( n - 1 ) ) + ( fib ( n - 2 ) )
+
+		fibh list = list ++ [ fib $ (length list) + 1 ]
+	in
+		last $ take n $ iterate fibh [1]
+
+-- Список треугольных чисел
+
+
+-- Список пирамидальных чисел
 
