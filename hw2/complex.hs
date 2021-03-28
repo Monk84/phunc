112f8bb3478a70b7039de44b4f0c92ac01c3b350
Monk <42776976+Monk84@users.noreply.github.com>
2021-03-29 00:09:06 +0300

    Update complex.hs

--- hw2/complex.hs
+++ hw2/complex.hs
@@ -7,4 +7,23 @@
 -- 2)	Создать реализацию класса типов Num для типа данных Complex
 
 -- 1
-Data Complex
+data Complex a = Complex a a deriving (Eq, Show)
+
+realPart :: (Complex a) -> a
+realPart (Complex a b) = a
+
+imagPart :: (Complex a) -> a
+imagPart (Complex a b) = b
+
+-- 2
+instance (Num a) => Num (Complex a) where
+  Complex a b + Complex c d = Complex (a+c) (b+d)
+  Complex a b - Complex c d = Complex (a-c) (b-d)
+  Complex a b * Complex c d = Complex (a*c - b*d) (a*d + b*c)
+  signum (Complex a b) = Complex (signum(a)) (signum(b))
+  abs (Complex a b ) = Complex (abs a) (abs b)
+  fromInteger n = Complex (fromInteger n) (fromInteger 0)
+
+-- 3) Определить экземпляры классов типов Functor и Applicative для типа данных Fun.
+
+-- 3
