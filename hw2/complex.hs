094f77e0e435a6318b9ee2f698d65aabba4917f4
Monk <42776976+Monk84@users.noreply.github.com>
2021-03-29 22:34:41 +0300

    Update complex.hs

--- hw2/complex.hs
+++ hw2/complex.hs
@@ -16,16 +16,19 @@ imagPart :: (Complex a) -> a
 imagPart (Complex a b) = b
 
 -- 2
-instance (Num a) => Num (Complex a) where
+instance (Floating a) => Num (Complex a) where
   Complex a b + Complex c d = Complex (a+c) (b+d)
   Complex a b - Complex c d = Complex (a-c) (b-d)
   Complex a b * Complex c d = Complex (a*c - b*d) (a*d + b*c)
-  signum (Complex a b) = Complex (signum(a)) (signum(b))
-  abs (Complex a b ) = Complex (abs a) (abs b)
+  abs (Complex a b) = Complex (sqrt(a*a + b*b)) ( 0 )
+  signum (Complex a b) = Complex ( a / sqrt(a*a + b*b)) ( b / sqrt(a*a + b*b) )
   fromInteger n = Complex (fromInteger n) (fromInteger 0)
-
--- abs&signum from Int or Integer - how&why?
 
 -- 3) Определить экземпляры классов типов Functor и Applicative для типа данных Fun.
 
 -- 3
+module Fun where
+  newtype Fun a b = Fun { getFun :: a -> b }
+  
+  instance Functor (Fun a) where
+    fmap f (Fun b) = Fun (f . b)
