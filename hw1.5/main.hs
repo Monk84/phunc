342a23cb35001ef82da95f16f24e7516395f0d29
Monk <42776976+Monk84@users.noreply.github.com>
2021-03-30 22:49:29 +0300

    delete haskell

--- hw1.5/main.hs
+++ hw1.5/main.hs
@@ -1,3 +1,9 @@
 -- Functor & Applicative for Either
-import Data.Either
+instance Functor (Either a) where
+  fmap g (Right x) = Right (g x)
+  fmap g (Left x) = Left x
 
+instance Applicative (Either a) where
+  pure x = Right x
+  Left g <*> x = Left g
+  Right g <*> x = fmap g x
